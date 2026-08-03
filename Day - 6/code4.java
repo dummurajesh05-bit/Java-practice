05ef7ddda5e65f65a9d45b7ef18d0298fb418cd9
dummurajesh05-bit <dummurajesh05@gmail.com>
2026-08-03 14:45:53 +0530

    Add files via upload

---
+++ Day - 6/code4.java
@@ -0,0 +1,13 @@
+class code4 {
+    public static void main(String[] args) {
+
+        int arr[][] = {
+                {1, 2, 3, 4},
+                {1, 2, 3, 4},
+                {1, 2, 3, 4},
+                {1, 2, 3, 4}
+        };
+
+        System.out.println(arr[1][1]);
+    }
+}
